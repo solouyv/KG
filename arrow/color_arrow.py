44efbb05a0e1cfdec1e0990bfaff0496ef4858d7
solouyv <solouyv@rambler.ru>
2021-04-10 16:34:04 +0300

    1.0.1

--- arrow/color_arrow.py
+++ arrow/color_arrow.py
@@ -4,16 +4,6 @@ from PIL import Image
 import numpy as np
 import arrow
 
-import sys
-import ipdb
-
-
-def run_debugger(type, value, tb):
-    ipdb.pm()
-
-
-sys.excepthook = run_debugger
-
 
 def is_black(pixel):
     if 0 in pixel:
@@ -32,29 +22,28 @@ def find_lines(array):
                         list_of_lines.append([k + 1, i])
                         in_figure = True
                 except IndexError:
-                    continue
+                    pass
             else:
                 try:
                     if not is_black(pixel) and is_black(row[k + 1]):
-                        list_of_lines.append([k, i])
+                        list_of_lines.append([k + 1, i])
                         in_figure = False
                 except IndexError:
-                    print(len(list_of_lines), len(list_of_lines) % 2)
                     if len(list_of_lines) % 2 != 0:
                         list_of_lines.pop()
-                    continue
+                        in_figure = False
     return list_of_lines
 
 
 def chenge_color(lst, img, color):
-    for i, item in enumerate(lst):
-        arrow.write_line(lst[0], lst[1], img, color)
-        del lst[0:2]
+    lll = range(len(lst))[::2]
+    for i in lll:
+        arrow.write_line(lst[i], lst[i + 1], img, color)
 
 
 if __name__ == '__main__':
     img = Image.open('arrow.bmp')
     array = np.asarray(img)
     lst = find_lines(array)
-    chenge_color(lst, img, (255, 0, 0))
-    img.save('arrow.bmp', 'BMP')
+    chenge_color(lst, img, (0, 159, 0))
+    img.save('green_arrow.bmp', 'BMP')
